f85e36e716f786701abe23158d3e5a6592cf6db8
Filippo Pesavento <pesaventofilippo@gmail.com>
2020-04-08 10:49:55 +0200

    Changed API library from urllib to requests
    Signed-off-by: Filippo Pesavento <pesaventofilippo@gmail.com>

--- modules/api.py
+++ modules/api.py
@@ -1,11 +1,10 @@
 from json import dumps, loads
 from re import sub
 from datetime import datetime, timedelta
-from json.decoder import JSONDecodeError
-from urllib.parse import quote_plus
-from urllib.request import Request, urlopen
-from urllib.error import HTTPError, URLError
 from http.client import RemoteDisconnected
+import requests
+from requests.exceptions import HTTPError, InvalidURL
+
 
 
 class AuthenticationFailedError(Exception):
@@ -32,19 +31,23 @@ class ClasseVivaAPI:
 
 
     def login(self, username: str, password: str):
-        url = self.baseApiUrl + "/auth/login/"
-        headers={"User-Agent":   "zorro/1.0",
-                 "Z-Dev-Apikey": "+zorro+",
-                 "Content-Type": "application/json"}
-        values=dumps({"uid": username,
-                      "pass": password})
+        url = self.baseApiUrl + "/auth/login"
+        headers = {
+            "User-Agent":   "zorro/1.0",
+            "Z-Dev-Apikey": "+zorro+",
+            "Content-Type": "application/json"
+        }
+        values = dumps({
+            "uid": username,
+            "pass": password
+        })
         data = values.encode('ascii')
-        req = Request(url, data, headers)
+
         try:
-            result = urlopen(req).read().decode('utf-8')
-        except (HTTPError, URLError, RemoteDisconnected):
+            req = requests.post(url, data, headers=headers)
+            result = req.json()
+        except (ValueError, HTTPError, InvalidURL, RemoteDisconnected):
             raise ApiServerError
-        result = loads(result)
 
         if 'authentication failed' in result.get('error', ''):
             raise AuthenticationFailedError
@@ -60,26 +63,29 @@ class ClasseVivaAPI:
         self.token = None
 
 
-    def _request(self, *path, method="GET", returnFile=False):
-        url = "{0}/students/{1}".format(self.baseApiUrl, self.id)
-        for x in path:
-            url += "/{0}".format(quote_plus(x))
+    def _request(self, relUrl, method="GET", returnFile=False):
+        url = "{0}/students/{1}/{2}".format(self.baseApiUrl, self.id, relUrl)
 
-        headers={"User-Agent": "zorro/1.0",
-                 "Z-Dev-Apikey": "+zorro+",
-                 "Z-Auth-Token": self.token,
-                 "Content-Type": "application/json"}
-        req = Request(url, headers=headers, method=method)
+        headers={
+            "User-Agent": "zorro/1.0",
+            "Z-Dev-Apikey": "+zorro+",
+            "Z-Auth-Token": self.token,
+            "Content-Type": "application/json"
+        }
         try:
-            result = urlopen(req)
-        except (HTTPError, URLError, RemoteDisconnected):
+            if method == "POST":
+                req = requests.post(url, headers=headers)
+            else:
+                req = requests.get(url, headers=headers)
+        except (HTTPError, InvalidURL, RemoteDisconnected):
             raise ApiServerError
 
         if returnFile:
-            return result
+            from io import BytesIO
+            return BytesIO(req.content)
 
         try:
-            jsonResult = loads(result.read().decode('utf-8'))
+            jsonResult = req.json()
             if jsonResult.get('error'):
                 if 'auth token expired' in jsonResult['error']:
                     raise AuthenticationFailedError
@@ -89,53 +95,53 @@ class ClasseVivaAPI:
                     raise InvalidRequestError
             return jsonResult
 
-        except JSONDecodeError:
-            return result.read().decode('utf-8')
+        except ValueError:
+            return req.text
 
 
     def assenze(self):
         now = datetime.now()
         if (now.month < 9) or (now.month == 9 and now.day < 10):
-            return self._request('absences', 'details', str(now.year - 1)+"0910")
-        return self._request('absences', 'details', str(now.year)+"0910")
+            return self._request("absences/details/{0}0910".format(now.year - 1))
+        return self._request("absences/details/{0}0910".format(now.year))
 
 
     def agenda(self, days: int=14):
-        return self._request('agenda', 'all', datetime.today().strftime("%Y%m%d"), (datetime.now() + timedelta(days=days)).strftime("%Y%m%d"))
+        return self._request("agenda/all/{0}/{1}".format(datetime.today().strftime("%Y%m%d"), (datetime.now() + timedelta(days=days)).strftime("%Y%m%d")))
 
 
     def didattica(self):
-        return self._request('didactics')
+        return self._request("didactics")
 
 
     def circolari(self):
-        return self._request('noticeboard')
+        return self._request("noticeboard")
 
 
     def info(self):
-        return self._request('cards')
+        return self._request("cards")
 
 
     def voti(self):
-        return self._request('grades')
+        return self._request("grades")
 
 
     def note(self):
-        return self._request('notes', 'all')
+        return self._request("notes/all")
 
 
     def materie(self):
-        return self._request('subjects')
+        return self._request("subjects")
 
 
     def lezioni(self, days: int=0):
-        return self._request('lessons', (datetime.now() + timedelta(days=days)).strftime("%Y%m%d"))
+        return self._request("lessons/{0}".format((datetime.now() + timedelta(days=days)).strftime("%Y%m%d")))
 
 
     def getFile(self, fileId: int):
-        return self._request('didactics', 'item', str(fileId), returnFile=True)
+        return self._request("didactics/item/{0}".format(fileId), returnFile=True)
 
 
     def getCirc(self, eventCode: int, pubId: int):
-        self._request('noticeboard', 'read', str(eventCode), str(pubId), '101', method="POST")
-        return self._request('noticeboard', 'attach', str(eventCode), str(pubId), '101', returnFile=True)
+        self._request("noticeboard/read/{0}/{1}/101".format(eventCode, pubId), method="POST")
+        return self._request("noticeboard/attach/{0}/{1}/101".format(eventCode, pubId), returnFile=True)
